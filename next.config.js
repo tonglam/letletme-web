ba59bc685e684ed6566ef84d691bead66c41933d
tong <bluedragon00000@gmail.com>
2025-03-14 21:40:02 +0800

    Refactor configuration files and update dependencies. Adjust `next.config.js` formatting for consistency, and update `package.json` and `package-lock.json` to reflect new dependency versions and optimizations.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-  // Disable font optimization since we're using static export
-  optimizeFonts: false,
-};
+	output: 'export',
+	eslint: {
+		ignoreDuringBuilds: true
+	},
+	images: {
+		unoptimized: true
+	}
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
