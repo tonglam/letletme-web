8256e56b520bd8b11b46db4d49455d20bbb9e498
tong <bluedragon00000@gmail.com>
2026-09-18 02:18:47 +0800

    fix: route invalid tournament IDs to explicit localized not-found page

--- proxy.ts
+++ proxy.ts
@@ -87,7 +87,7 @@ function invalidRouteResponse(
 	i18nResponse: NextResponse
 ) {
 	const url = req.nextUrl.clone()
-	url.pathname = `/${locale}/__not-found`
+	url.pathname = `/${locale}/route-not-found`
 	url.search = ''
 	return copyCookies(
 		i18nResponse,
